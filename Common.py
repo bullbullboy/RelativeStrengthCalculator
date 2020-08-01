14a5bfd535a1acebd0bd92ec5c83ab7f8fb8af79
bullbullboy <gtrewqasdf@gmail.com>
2020-08-01 09:38:21 +0900

    [仕様変更]結果のテキストファイル名を変更(resultPrice.txt)

--- Common.py
+++ Common.py
@@ -1,7 +1,7 @@
 import os
 
 def pathOfPriceResult():
-    return os.path.dirname(__file__) + '/result.txt'
+    return os.path.dirname(__file__) + '/resultPrice.txt'
 
 def pathOfRSResult():
     return os.path.dirname(__file__) + '/resultRS.txt'
